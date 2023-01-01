fe3f4d17ed699e40bf7becdfb1bd7ff763429a76
sumit-krk <kushakk007@gmail.com>
2023-01-01 22:34:00 +0530

    import and export in node js

---
+++ NEM(L-2)/data.js
@@ -0,0 +1,5 @@
+const [sum,sub,mul,diff]=require('./index')
+console.log(sum(20,40));
+console.log(sub(40,20));
+console.log(mul(20,2));
+console.log(diff(40,4));
---
+++ NEM(L-2)/index.js
@@ -0,0 +1,13 @@
+const sum=(a,b)=>{
+    return a+b;
+}
+const sub=(a,b)=>{
+    return a-b;
+}
+const mul=(a,b)=>{
+    return a*b;
+}
+const diff=(a,b)=>{
+    return a/b;
+}
+module.exports=[sum,sub,mul,diff];
