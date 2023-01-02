f77d7897622defcb504bcac11bdb4b8e9338445b
sumit-krk <kushakk007@gmail.com>
2023-01-02 21:32:26 +0530

    read json file using node js

--- NEM(Day-4)/index.js
+++ NEM(Day-4)/index.js
@@ -1,14 +1,27 @@
+const { readFile } = require('fs');
 const http=require('http');
+const fs=require('fs');
 
 const server=http.createServer((request,response)=>{
     if(request.url==="/"){
         response.end("This is home page");
     }
     else if(request.url==="/data"){
-        response.end("Data will be here");
+        fs.readFile("./data.json",(err,data)=>{
+            if(err){
+                response.write(err);
+                response.end();
+            }
+            else{
+                response.end(data);
+            }
+        })
+    }
+    else{
+        response.end("Invalid Url");
     }
 })
 
-server.listen(4500,()=>{
+server.listen(4600,()=>{
     console.log("server listning on port 4500");
 })
