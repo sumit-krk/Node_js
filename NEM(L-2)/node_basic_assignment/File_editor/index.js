422621d3615566ea9dfc7428e1c9c94b1f0e7da0
sumit-krk <kushakk007@gmail.com>
2023-01-06 01:47:15 +0530

    append the content in txt file using cli

--- NEM(L-2)/node_basic_assignment/File_editor/index.js
+++ NEM(L-2)/node_basic_assignment/File_editor/index.js
@@ -1,13 +1,30 @@
 const fs=require('fs');
 const arguments=process.argv;
+// if(arguments[2]=="read"){
+//     fs.readFile(`./${arguments[3]}`,{encoding:'utf-8'},(err,data)=>{
+//         if(err){
+//             console.log("file cannot be read");
+//             console.log(err);
+//         }
+//         else{
+//             console.log(data);
+//         }
+//     })
+// }
+console.log(arguments)
 if(arguments[2]=="read"){
-    fs.readFile(`./${arguments[3]}`,{encoding:'utf-8'},(err,data)=>{
-        if(err){
-            console.log("file cannot be read");
-            console.log(err);
-        }
-        else{
-            console.log(data);
-        }
-    })
+    let data;
+    try{
+        data=fs.readFileSync(`./${arguments[3]}`,{encoding:'utf-8'});
+        console.log(data);
+    }catch(err){
+        console.log(err)
+    }
+}
+else if(arguments[2]=="append"){
+    try{
+        fs.appendFileSync(`./${arguments[4]}`,`${arguments[3]}\n`);
+    }catch(err){
+        console.log(err)
+    }
 }
