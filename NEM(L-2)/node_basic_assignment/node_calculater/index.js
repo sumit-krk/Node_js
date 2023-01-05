640938b20be04a79f62633846269d990c1c534ba
sumit-krk <kushakk007@gmail.com>
2023-01-05 19:02:51 +0530

    added sin,cos,tan in nodejs calculater

--- NEM(L-2)/node_basic_assignment/node_calculater/index.js
+++ NEM(L-2)/node_basic_assignment/node_calculater/index.js
@@ -20,6 +20,18 @@ else if(arguments[2]=='random'){
     var random=ran(arguments[3],arguments[4])
     console.log(`random number between ${arguments[3]} and ${arguments[4]} is ${random}`);
 }
+else if(arguments[2]=='sin'){
+    var sin=sinT(arguments[3]);
+    console.log(`value is sin${arguments[3]} is ${sin}`);
+}
+else if(arguments[2]=='cos'){
+    var cos=cosT(arguments[3]);
+    console.log(`value is cos${arguments[3]} is ${cos}`);
+}
+else if(arguments[2]=='tan'){
+    var tan=tanT(arguments[3]);
+    console.log(`value is tan${arguments[3]} is ${tan}`);
+}
 else{
     console.log('Invalid input');
 }
@@ -41,4 +53,13 @@ function div(a, b) {
 }
 function ran(a,b){
     return randomInt(parseInt(a),parseInt(b));
+}
+function sinT(a){
+    return Math.sin(parseInt(a));
+}
+function cosT(a){
+    return Math.cos(parseInt(a));
+}
+function tanT(a){
+    return Math.tan(parseInt(a));
 }
