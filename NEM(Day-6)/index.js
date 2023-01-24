a9042d84a4c272c60f4555130b05c6f2a543bba2
sumit-krk <kushakk007@gmail.com>
2023-01-24 17:08:17 +0530

    build my own timer middleware

--- NEM(Day-6)/index.js
+++ NEM(Day-6)/index.js
@@ -1,15 +1,25 @@
 const express=require("express");
 const app=express();
+const fs=require("fs");
 
-app.use((req,res,next)=>{
-    if(req.url=='/about'){
-        console.log("You are in correct route");
-        next();
-    }
-    else{
-        res.send("Route no avilable")
-    }
-})
+// app.use((req,res,next)=>{
+//     if(req.url=='/about'){
+//         console.log("You are in correct route");
+//         next();
+//     }
+//     else{
+//         res.send("Route no avilable")
+//     }
+// })
+
+const TimeLogger=(req,res,next)=>{
+    let startTime=new Date().getTime();
+    next();
+    let endTime=new Date().getTime();
+    console.log(endTime-startTime)
+}
+
+app.use(TimeLogger)
 
 app.get("/",(req,res)=>{
     res.send("I am home page")
@@ -18,6 +28,11 @@ app.get("/",(req,res)=>{
 app.get("/about",(req,res)=>{
     console.log("about page")
     res.send("<h1>This is about page</h1>")
+})
+
+app.get("/data",(req,res)=>{
+    let data=fs.readFileSync("./dummy.txt","utf-8")
+    res.send(data);
 })
 
 app.listen(3600,()=>{
