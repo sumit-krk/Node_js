df758640de260fbb5daad53477c32b3783ae0e00
sumit-krk <kushakk007@gmail.com>
2023-02-04 01:20:51 +0530

    login and logout functionlity

--- NEM(Day-10)/index.js
+++ NEM(Day-10)/index.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const { connection } = require('./db')
+const { userModel } =require('./models/user.model')
 
 const app=express();
 
@@ -9,12 +10,33 @@ app.get("/",(req,res)=>{
     res.send("Home page");
 })
 
-app.post("login",()=>{
-    res.send("Logdin");
+app.post("/login",async(req,res)=>{
+    const {email,password}=req.body;
+   try{
+    const user=await userModel.find({email,password})
+    if(user.length>0){
+        res.send("logdin sussefully")
+    }else{
+        res.send("No valid user");
+    }
+    console.log(user);
+   }catch(err){
+        console.log("something went wrong");
+        console.log(err);
+   }
 })
 
-app.post("/register",(req,res)=>{
-    res.send("registered");
+app.post("/register",async(req,res)=>{
+    const payload=req.body
+    try{
+        const user=new userModel(payload)
+        await user.save()
+        res.send("registered");
+    }
+    catch(err){
+        console.log("something wrong in register")
+        console.log(err)
+    }
 })
 
 app.listen(4000,async()=>{
